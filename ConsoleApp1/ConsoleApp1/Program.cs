27e3250670c0489ea25cdeb620c5eed07d54a422
Kabryn Hodgson <kjhodgson@tlu.edu>
2017-10-03 22:00:13 -0500

    I have created the parallel verion of the program.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MonteCarloPiApproximation
@@ -8,7 +9,7 @@ namespace MonteCarloPiApproximation
     {
         // Enviroment will grab the number of cores, will help to put things in parallel.
         static int numberOfCores = Environment.ProcessorCount;
-        static int iterations = 60000000 * numberOfCores;
+        static int iterations = 10000000 * numberOfCores;
 
         static void Main(string[] args)
         {
@@ -20,17 +21,28 @@ namespace MonteCarloPiApproximation
             var sw = new Stopwatch();
             sw.Start();
 
-            Console.WriteLine("\nRunning the Method for approximating pi:");
+            Console.WriteLine("\nRunning the Method for approximating pi without parallelism:");
 
             MonteCarloPiApproximation();
 
-            Console.WriteLine("Running program without parallelism: (time in ms)" + sw.ElapsedMilliseconds);
+            Console.WriteLine("Elasped time: (time in ms)" + sw.ElapsedMilliseconds);
             long timeSeconds = sw.ElapsedMilliseconds / 1000;
             Console.WriteLine("\nTime in seconds:" + timeSeconds);
 
             sw.Stop();
+            sw.Start();
 
-            Console.WriteLine("Press Enter Key");
+            Console.WriteLine("\nNow running the program with parallelism");
+
+            MonetCarloPiParallel();
+
+            Console.WriteLine("Elasped time: (time in ms)" + sw.ElapsedMilliseconds);
+            timeSeconds = sw.ElapsedMilliseconds / 1000;
+            Console.WriteLine("\nTime in seconds:" + timeSeconds);
+
+            sw.Stop(); 
+
+            Console.WriteLine("The program is done, please press a key to exit!");
             Console.ReadKey();
         }
 
@@ -61,20 +73,42 @@ namespace MonteCarloPiApproximation
         private static void MonetCarloPiParallel()
         {
             double piApprox = 0;
-            int total = 0;
-            int inCircle = 0;
+            int TotalinCircle = 0;
+            
             double x = 0;
             double y = 0;
-            Random rand = new Random();
-
-            // Need to figure out how to seperate tasks for the different cores. 
 
             int[] coreCounts = new int[numberOfCores];
             Task[] tasks = new Task[numberOfCores];
 
-            // Need to find out if creating seperate tasks equal to the number of cores will force the cores to take the seperate tasks. 
+            // This will seperate the number of approximations needed among the different number of cores that a computer has
+            for (int i = 0; i < numberOfCores; i++)
+            {
+                // Corenumber is used here because of how the task functions in accordance to the loop. It stores the loop iterations so that the count can be properly placed.
+                int coreNumber = i;
+                tasks[coreNumber] = Task.Factory.StartNew(() =>
+                {
+                    int taskInCircle = 0;
 
+                    // For loop used to divided the work among the cores. 
+                    for (int j = 0; j < iterations / numberOfCores; j++)
+                    {
+                        
+                        x = new Random().NextDouble();
+                        y = new Random().NextDouble();
+                        if ((Math.Sqrt((x * x) + (y * y)) <= 1.0)) // <= 1 implies it is in the circle, anything outside is discarded but counted
+                            taskInCircle++;
+                    }
+                    coreCounts[coreNumber] = taskInCircle;
+                });
+               
+            }
+            // Needed for ending the tasks or they will continue to run in background.....
+            Task.WaitAll(tasks);
+            TotalinCircle = coreCounts.Sum();
 
+            piApprox = 4 * ((double)TotalinCircle / (double)iterations);
+            Console.WriteLine("\nApproximated Pi = {0}", piApprox);
         }
 
 
